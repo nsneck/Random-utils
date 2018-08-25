d39fb6035c5f67099180e85cc538e26324ff300a
Nico Sneck <snecknico@gmail.com>
2018-08-25 14:09:22 +0300

    tarsplit: better arg parsing, add output dir arg
    Also fix indexing bug for new archive creation.

--- tarsplit.py
+++ tarsplit.py
@@ -1,29 +1,46 @@
 #!/usr/bin/env python3
 
-import sys, io, os.path
-import tarfile
+import sys, io, os.path, tarfile, argparse
 
-usagetext = 'This script takes two arguments. First the path to an archive file you want to split, ' \
-'and then the number of files you want to have your archive be split by.'
-if len(sys.argv) < 3:
-    print(usagetext)
+class ArgumentParser(argparse.ArgumentParser):
+    # We define a custom parser class in order to be able to override the default
+    # error behavior. We want to print the help text whenever the user makes a mistake
+    # so they can get a more informative help message.
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+argparser = ArgumentParser(description='Split a tar archive into smaller archives by number of files inside each split archive.')
+argparser.add_argument('splitnum', metavar='N', type=int, help='The number of files you want to have each split archive be populated by.')
+argparser.add_argument('tarpath', metavar='I', type=str, help='The input tar archive that will be split to smaller archives.')
+argparser.add_argument('-o, --outputdir', dest='outputdir', default=os.path.dirname(os.path.abspath(__file__)), type=str,
+                    help='The output directory where split archives will be written to. Default is script execution directory.')
+args = argparser.parse_args()
+splitnum = args.splitnum
+tarpath = args.tarpath
+outputdir = args.outputdir
+if not splitnum > 0:
+    print("error: The first argument must be the number of files you want to have your archive split by, it must be positive and greater than 0.\n")
+    argparser.print_help()
     exit()
-tarpath = sys.argv[1]
-splitnum = sys.argv[2]
 try:
     tarfile.is_tarfile(tarpath)
 except FileNotFoundError:
     print("Couldn't find an archive with the path '{}'.".format(tarpath))
     exit()
-if not splitnum.isdigit() or not int(splitnum) > 0:
-    print("The second argument must be the number of files you want to have your archive split by, it must be positive and greater than 0.")
+# Without a trailing '/' file handling inside the directory won't work
+if not outputdir.endswith('/'):
+    outputdir += '/'
+if not os.path.isdir(outputdir):
+    print("error: '{}' is not a directory.\n".format(outputdir))
+    argparser.print_help()
     exit()
 archivename = os.path.splitext(os.path.basename(tarpath))[0] # just pick up the archive filename without path / extension
 tar = tarfile.open(tarpath, 'r' or 'r:*')
 newtar = None
 splitnumcounter = 1
 splitarchivenum = 1
-newtarname = archivename + '_0.tar'
+newtarname = outputdir + archivename + '_0.tar'
 
 while True: # infinite loop, we break out of it once we've processed all TarFile members
     member = tar.next()
@@ -34,14 +51,14 @@ while True: # infinite loop, we break out of it once we've processed all TarFile
     if splitnumcounter > int(splitnum):
         newtar.close()
         newtar = None # reset the newtar var so we create a new one with a new name
-        splitnumcounter = 0
-        newtarname = archivename + '_' + str(splitarchivenum) + '.tar'
+        splitnumcounter = 1
+        newtarname = outputdir + archivename + '_' + str(splitarchivenum) + '.tar'
         splitarchivenum += 1
     if newtar is None:
         newtar = tarfile.open(newtarname, 'a' or 'a:')
-    file = tar.extractfile(member)
-    file = io.BytesIO(file.read()) # I couldn't find a better way to perform this in-memory, so this'll do.
-    newtar.addfile(member, file)
+    extractedfile = tar.extractfile(member)
+    extractedfile = io.BytesIO(extractedfile.read()) # I couldn't find a better way to perform this in-memory, so this'll do.
+    newtar.addfile(member, extractedfile)
     splitnumcounter += 1
 if newtar is not None:
     newtar.close()
